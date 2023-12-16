09bf343b98d1ea37cecba307191355ab001ef3a6
jorgemochonbernalfullstack <jorgemochonbernallopez@gmail.com>
2023-12-16 13:56:35 +0100

    fix archivo persistencia

--- app.py
+++ app.py
@@ -2,16 +2,13 @@
 from flask import Flask, request, redirect
 from persistencia.persistencia import is_registrado
 
-"""Módulo principal de la aplicación web"""
 
 app = Flask(__name__)
 
 
 @app.route("/pizza", methods=["POST"])
 def prepara_pedido():
-    """
-    Maneja solicitudes POST para preparar pedidos.
-    """
+    """Maneja solicitudes POST para preparar pedidos"""
     cliente_nombre = request.form.get("cliente_nombre")
     cliente_apellido = request.form.get("cliente_apellido")
     print(f"Nombre: {cliente_nombre}, Apellido: {cliente_apellido}")
--- persistencia/persistencia.py
+++ persistencia/persistencia.py
@@ -11,13 +11,8 @@ def is_registrado(nombre, apellido):
         print("Ocurrió un error al registrar los pedidos !!!")
 
 
-"""Metodo que verifica si el nombre y el apellido no están vacíos"""
-
-
 def recorrer_pedidos(nombre, apellido):
-    """
-    Verifica si el nombre y el apellido no están vacíos.
-    """
+    """Verifica si el nombre y el apellido no están vacíos."""
     if nombre in ("") or apellido in (""):
         print("Error al registrar el pedido.")
         return False
@@ -25,12 +20,7 @@ def recorrer_pedidos(nombre, apellido):
     return True
 
 
-"""Metodo que regista, guarda y crea el pedido con sus nombres y apellidos"""
-
-
 def guardar_pedido(nombre, apellidos):
-    """
-    Registra, guarda y crea el pedido con los nombres y apellidos especificados.
-    """
+    """Registra, guarda y crea el pedido con los nombres y apellidos especificados"""
     with open("pedidos.txt", "a", encoding="utf-8") as file:
         file.write("-" + nombre + " " + apellidos + "\n")
